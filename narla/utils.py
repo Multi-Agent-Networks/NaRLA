479d9ce35da5e7a2cb16c60ee5ee5394565e497a
Jordan Ott <jordanott365@gmail.com>
2021-01-30 16:59:49 -0500

    Helper functions

--- narla/utils.py
+++ narla/utils.py
@@ -1,3 +1,10 @@
+"""
+Utils
+---
+Supporting functions for NaRLA
+"""
+
+import argparse
 import tensorflow as tf
 import tensorflow_probability as tfp
 
@@ -12,3 +19,22 @@ def pg_action_loss(prob, action, reward):
     loss = -log_prob*reward
 
     return loss
+
+def prepare_state(state):
+    return tf.convert_to_tensor(
+        state.reshape(1, -1),
+        dtype=tf.float32
+    )
+
+def parse_args():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--plot', action='store_true')
+    parser.add_argument('--update_every', type=int, default=25)
+    parser.add_argument('--num_episodes', type=int, default=100000)
+
+    parser.add_argument('--num_nodes', type=int, default=15)
+    parser.add_argument('--num_layers', type=int, default=2)
+    parser.add_argument('--reward_type', default='task', choices=['all','task','bio','bio_then_all'])
+
+    return parser.parse_args()
