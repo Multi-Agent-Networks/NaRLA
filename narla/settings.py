35d58dad1797fb7366c32f380cc3c46cba83e6ba
jordanott <jordanott365@gmail.com>
2023-02-19 16:33:43 -0500

    Constants for all: neuron types; saved data; reward types

--- narla/settings.py
+++ narla/settings.py
@@ -1,14 +1,19 @@
+from __future__ import annotations
+
 import tap
+import narla
 from typing import Literal
+from narla.neurons import neuron_types
+from narla.history import reward_types
 
 
 class Settings(tap.Tap):
     batch_size: int = 128
     device: Literal["cpu", "cuda"] = "cuda"
     environment: Literal["cart-pole"] = "cart-pole"
-    neuron_type: Literal["deep_q", "policy_gradient"] = "deep_q"
+    neuron_type: Literal[neuron_types.DEEP_Q, neuron_types.ACTOR_CRITIC] = neuron_types.DEEP_Q
     number_of_layers: int = 2
-    number_of_neurons: int = 15
+    number_of_neurons_per_layer: int = 15
     update_every: int = 1
     render: bool = False
-    reward_type: Literal["all", "task", "bio", "bio_then_all"] = "all"
+    reward_type: Literal[reward_types.TASK_REWARD] = reward_types.TASK_REWARD
