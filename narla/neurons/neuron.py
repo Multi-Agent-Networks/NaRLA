57dfa4763b99958f16875c08b96fc9f9c14f914b
jordanott <jordanott365@gmail.com>
2023-02-21 19:43:45 -0500

    Making `Neuron` Abstract

--- narla/neurons/neuron.py
+++ narla/neurons/neuron.py
@@ -1,19 +1,22 @@
 from __future__ import annotations
 
+import abc
 import torch
 import narla
 
 
-class Neuron:
+class Neuron(metaclass=abc.ABCMeta):
     def __init__(self, observation_size: int, number_of_actions: int):
         self.observation_size = observation_size
         self.number_of_actions = number_of_actions
 
         self._history = narla.history.History()
 
+    @abc.abstractmethod
     def act(self, observation: torch.Tensor) -> torch.Tensor:
         pass
 
+    @abc.abstractmethod
     def learn(self):
         pass
 
